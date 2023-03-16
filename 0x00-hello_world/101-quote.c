f32b429f71abcf03c1ee228d684a48583526e354
fegougholehpf <fegougholehpf@gmail.com>
2023-03-16 13:12:23 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,8 @@
  * Return:Always 0 (Success)
 */
 int main(void)
-{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, str1, 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
